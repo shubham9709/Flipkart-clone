fed63442c35770b4fafa65fdd8990ec1d7c0523b
Shubham <shubham@pop-os.localdomain>
2023-03-23 17:29:19 +0530

    Deleting the mistakenly made flipkart repo in Flipkart repo

--- server/controller/product-controller.js
+++ server/controller/product-controller.js
@@ -1,4 +1,6 @@
+import { request } from "express";
 import Product from "../model/product-schema.js";
+
 const getProducts = async (request,response) => {
   try{
     const products =await Product.find();
@@ -8,4 +10,15 @@ const getProducts = async (request,response) => {
     response.status(500).json({message:error.message});
   }
 }
+
+export const getProductByID = async (request,response) => {
+  try {
+    const id = request.params.id;
+    const product = await Product.findOne({'id': id});
+    response.status(200).json(product);
+  } catch (error) {
+    response.status(500).json({message:error.message});
+  }
+}
+
 export default getProducts;
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import getProducts from "../controller/product-controller.js";
+import getProducts, { getProductByID } from "../controller/product-controller.js";
 import { userSignup, userLogIn } from "../controller/user-controller.js";
  
 const router = express.Router();
@@ -7,5 +7,5 @@ const router = express.Router();
 router.post('/signup',userSignup);
 router.post('/login',userLogIn);
 router.get('/products', getProducts);
-
+router.get('/product/:id',getProductByID);
 export default router;
