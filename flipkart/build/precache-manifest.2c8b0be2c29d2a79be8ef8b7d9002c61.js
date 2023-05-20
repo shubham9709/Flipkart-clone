50ea70a2b0829ad133071c573ba4f46498dee730
Shubham <shubham@pop-os.localdomain>
2023-05-20 22:05:11 +0530

    DEploymrnt test

---
+++ flipkart/build/precache-manifest.2c8b0be2c29d2a79be8ef8b7d9002c61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "14c681bbfeb8f279d9b7",
+    "url": "/static/js/main.14c681bb.chunk.js"
+  },
+  {
+    "revision": "e994630d53f39878cfc3",
+    "url": "/static/css/1.e343b949.chunk.css"
+  },
+  {
+    "revision": "e994630d53f39878cfc3",
+    "url": "/static/js/1.e994630d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "e87f72d26a1e5f44acb3ad44c8ae4ac5",
+    "url": "/index.html"
+  }
+];
